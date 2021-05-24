d19579c847f7eed5b52046f83cc6a250de51f88f
TamNguyen <ngductam91@gmail.com>
2021-05-24 14:30:44 +0700

    Validate Email Form

--- Validate-Email/index.php
+++ Validate-Email/index.php
@@ -2,7 +2,8 @@
 
 function checkEmail($string)
 {
-    $pattern = "/^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)/";
+//    $pattern = "/^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\.[A-Za-z0-9]+)/";
+    $pattern = "/^\w+\w*@\w+(\.\w+)/";
     if(preg_match($pattern,$string)){
         echo "Validate Email <br>";
     }else{
