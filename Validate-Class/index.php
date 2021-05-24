12ba7aa7ecf721d96fb059a4abfa91ae2b89b991
TamNguyen <ngductam91@gmail.com>
2021-05-24 14:17:15 +0700

    Validate Class Name

--- Validate-Class/index.php
+++ Validate-Class/index.php
@@ -2,7 +2,7 @@
 
 function checkClass($string)
 {
-    $pattern = "/^[C|A|P][0-9]{4}[G-M][0-9]$/";
+    $pattern = "/^[CAP][0-9]{4}[G-M][0-9]$/";
     if (preg_match($pattern, $string)) {
         echo "Validate Class Name <br>";
     } else {
